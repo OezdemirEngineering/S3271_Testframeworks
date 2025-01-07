32827a7bceb96914a00c39fa5a158c6dd4cab416
Caglar Özdemir <c.oezdemir@oeeng.de>
2025-01-07 15:38:56 +0100

    added Flaui Login Logout Test

--- LoginApp.Integration/LoginWithFlauiTest.cs
+++ LoginApp.Integration/LoginWithFlauiTest.cs
@@ -1,28 +1,48 @@
 ﻿using FlaUI.Core.AutomationElements;
 using FlaUI.UIA3;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace LoginApp.Integration;
 
-public class LoginWithFlauiTest
+public class LoginWithFlauiTest : IDisposable
 {
+    public Window _mainWindow;
+
+    public LoginWithFlauiTest()
+    {
+        var process = Process.Start("LoginApp.exe");
+        var automation = new UIA3Automation();
+        var app = FlaUI.Core.Application.Attach(process);
+        _mainWindow = app.GetMainWindow(automation).WaitUntilEnabled(new TimeSpan(0, 0, 0, 0, 100));
+    }
+
+
     [Fact]
     public void Login_UserNamePassword_Dashboard()
     {
         // Arrange 
-        var process = Process.Start("LoginApp.exe");
-        var automation = new UIA3Automation();
-        var app = FlaUI.Core.Application.Attach(process);
-        var mainWindow = app.GetMainWindow(automation).WaitUntilEnabled(new TimeSpan(0, 0, 0, 0, 100));
+        var userNameTextBox = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("UserName")).AsTextBox();
+        var passwordTextBox = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("Password")).AsTextBox();
+        var loginButton = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("LoginButton")).AsButton();
 
-        var userNameTextBox = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("UserName")).AsTextBox();
+        // Act
         userNameTextBox.Text = "admin";
+        passwordTextBox.Text = "admin";
+        loginButton.Click();
 
 
+        // Assert
+        var dashboard = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("DashBoardTitle")).AsTextBox();
+       
+        dashboard.Should().NotBeNull();
+    }
+
+    public void Dispose()
+    {
+        _mainWindow.Close();
     }
 }
